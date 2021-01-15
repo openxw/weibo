0a10f39fdf70903143543382b7aaa903f7256407
openxw <openxw@qq.com>
2021-01-15 02:37:36 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,16 +7,23 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    //
     public function create()
     {
-        # code...
         return view('users.create');
     }
 
     public function show(User $user)
     {
-        # code...
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
